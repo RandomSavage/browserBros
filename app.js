a2aa5b035cf9c263e397d349e6060903cf2011f9
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2021-06-07 00:00:31 -0400

    Creating Custom form in #form-three

--- app.js
+++ app.js
@@ -14,3 +14,31 @@ console.log(inputFrame)
 
 let dateFrame = document.getElementById('dateFrame')
 dateFrame.placeholder = inputFrame
+
+let myBtn = document.getElementById('myBtn')
+
+let myFunK = () => {
+  let myMonth = document.querySelector('.mizunth')
+
+  myMonth.style.backgroundColor = "green"
+}
+
+let dateCollector = []
+ let collateDate = (e) => {
+   e.preventDefault()
+   let myForm = document.getElementById("customForm")
+   let myDay = document.getElementsByName('day')
+   let myYear = document.getElementsByName('year')
+   let displayDate = `${myForm.month.value}/${myForm.day.value}/${myForm.year.value}`
+   let checkDate = document.createElement('h1')
+   checkDate.innerText = displayDate
+   myForm.append(checkDate)
+   console.log(displayDate)
+
+  myFunK()
+
+   dateCollector.push(myForm.month.value, myForm.day.value, myForm.year.value)
+   console.log(dateCollector)
+  
+ }
+myBtn.addEventListener('click', collateDate)
